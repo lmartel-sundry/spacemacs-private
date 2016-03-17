b4804819a773ee0349267e3f596bf66a09703bff
Leo Martel <leo@asana.com>
2016-03-16 18:51:35 -0700

    add fireplace layer

---
+++ fireplace/extensions.el
@@ -0,0 +1,35 @@
+;;; extensions.el --- fireplace Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq fireplace-pre-extensions
+      '(
+        ;; pre extension names go here
+        ))
+
+(setq fireplace-post-extensions
+      '(
+        ;; post extension names go here
+        load-fireplace
+        ))
+
+;; For each extension, define a function fireplace/init-<extension-name>
+;;
+;; (defun fireplace/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+(defun fireplace/init-load-fireplace ()
+  (load "~/.emacs.d/private/fireplace/emacs-fireplace/fireplace.elc"))
+
